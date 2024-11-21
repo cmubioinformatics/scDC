c6abd6ed450a916218ee756e3f10725b6396d620
Xin Zhang <lmulittlechild@outlook.com>
2024-11-21 16:51:01 +0100

    Update utils.R
    change the scran_high_var function, fitTrendVar methods

--- R/utils.R
+++ R/utils.R
@@ -26,7 +26,10 @@
 
 .scran_high_var <- function(exprsMat,topn=1000){
   topn <- min(topn, nrow(exprsMat))
-  var.fit <- scran::fitTrendVar(exprsMat, method="loess")
+  # var.fit <- scran::fitTrendVar(exprsMat, method="loess")
+  means <- rowMeans(logcounts(sce))
+  vars <- rowVars(logcounts(sce))
+  var.fit <- scran::fitTrendVar(means, vars, lowess=TRUE)
   var.out <- scran::decomposeVar(exprsMat, var.fit)
   hvg.out <- var.out[order(var.out$bio, decreasing=TRUE)[1:topn], ]
   return(rownames(hvg.out))
